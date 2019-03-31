debebde10965b2bee6c5a8555587140ea7e07ef1
alexs55 <alex.acosta.dv@gmail.com>
2019-03-30 21:18:54 -0500

    connection to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,24 @@
 var mysql= require("mysql");
 
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: 'root',
-    password: 'Rayquazayuna8703',
-    database: 'burgers_db'
-});
+ 
+
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+
+}else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: 'root',
+        password: 'Rayquazayuna8703',
+        database: 'burgers_db'
+    });
+}
+
 
 connection.connect(function(err) {
     if (err) throw err;
